f7ec834be727d7f055fc825db7a9518b5ddaf8a9
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2023-12-08 11:27:09 +0100

    Update Monitoring.class.php

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -651,7 +651,7 @@ class Monitoring extends eqLogic {
 			}
 			else {
 				if ($pass == '') {
-					$pass = $sshpubkey;
+					$pass = PublicKeyLoader::load($sshpubkey);
 				}
 				if (!$sshconnection->login($user, $pass)) {
 					log::add('Monitoring', 'error', 'Authentification SSH KO pour '.$equipement);
