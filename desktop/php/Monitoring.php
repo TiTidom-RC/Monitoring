18232ff40a061afa10a6d214711fba60f53e0c14
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2023-12-09 19:20:21 +0100

    Update Monitoring.php

--- desktop/php/Monitoring.php
+++ desktop/php/Monitoring.php
@@ -159,11 +159,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                     </div>
                                     <div class="form-group">
                                         <label class="col-md-4 control-label">{{Clé SSH}}</label>
-                                        <div class="col-md-6 input-group">
+                                        <div class="col-md-6">
                                             <textarea type="text" autocomplete="ssh-key" class="eqLogicAttr form-control autogrow roundedLeft" data-l1key="configuration" data-l2key="ssh-key" placeholder="{{Saisir la clé SSH}}"></textarea>
-                                            <span class="input-group-btn">
-											    <a class="btn btn-default form-control bt_showPass roundedRight"><i class="fas fa-eye"></i></a>
-										    </span>
                                         </div>
                                     </div>
                                 </div>
