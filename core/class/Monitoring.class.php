287abd6343c527cdd97a03d71707b94566048eb1
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2023-12-15 09:17:03 +0100

    Update Monitoring.class.php

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -1733,8 +1733,7 @@ class Monitoring extends eqLogic {
 						switch ($paramaction) {
 							case "reboot":
 								try {
-									// $rebootcmd = "sudo shutdown -r now >/dev/null & shutdown -r now >/dev/null";
-									$rebootcmd = "sudo /sbin/shutdown -r now >/dev/null";
+									$rebootcmd = "sudo /sbin/shutdown -r now >/dev/null & /sbin/shutdown -r now >/dev/null";
 									$sshconnection->exec($rebootcmd);
 								} catch (Exception $e) {
 									log::add('Monitoring','debug','[SYNO-REBOOT] Exception [REBOOT] :: '. $equipement .' :: '. $e->getMessage());	
@@ -1743,9 +1742,8 @@ class Monitoring extends eqLogic {
 								break;
 							case "poweroff":
 								try {
-									// $poweroffcmd = 'sudo /sbin/shutdown -h now >/dev/null & /sbin/shutdown -h now >/dev/null';
 									// $poweroffcmd = 'sudo /sbin/shutdown -P now >/dev/null & /sbin/shutdown -P now >/dev/null';
-									$poweroffcmd = 'sudo /sbin/shutdown -h now >/dev/null';
+									$poweroffcmd = 'sudo /sbin/shutdown -h now >/dev/null & /sbin/shutdown -h now >/dev/null';
 									$sshconnection->exec($poweroffcmd);
 								} catch (Exception $e) {
 									log::add('Monitoring','debug','[SYNO-OFF] Exception [POWEROFF] :: '. $equipement .' :: '. $e->getMessage());	
@@ -1758,7 +1756,7 @@ class Monitoring extends eqLogic {
 						switch ($paramaction) {
 							case "reboot":
 								try {
-									// $rebootcmd = "sudo shutdown -r now >/dev/null";
+									// $rebootcmd = "sudo shutdown -r now >/dev/null & shutdown -r now >/dev/null";
 									$rebootcmd = "sudo reboot >/dev/null & reboot >/dev/null";
 									$sshconnection->exec($rebootcmd);
 								} catch (Exception $e) {
@@ -1785,12 +1783,12 @@ class Monitoring extends eqLogic {
 			if($this->getConfiguration('synology') == '1'){
 				switch ($paramaction) {
 					case "reboot":
-						$rebootcmd = "sudo /sbin/shutdown -r now >/dev/null";
+						$rebootcmd = "sudo /sbin/shutdown -r now >/dev/null & /sbin/shutdown -r now >/dev/null";
 						log::add('Monitoring','info','[SYNO-REBOOT] Lancement commande locale REBOOT :: '. $equipement);
 						exec($rebootcmd);
 						break;
 					case "poweroff":
-						$poweroffcmd = 'sudo /sbin/shutdown -h now >/dev/null';
+						$poweroffcmd = 'sudo /sbin/shutdown -h now >/dev/null & /sbin/shutdown -h now >/dev/null';
 						log::add('Monitoring','info','[SYNO-OFF] Lancement commande locale POWEROFF :: '. $equipement);
 						exec($poweroffcmd);
 						break;
