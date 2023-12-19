130878a1c45d589b72bc2ec018d8684290f59aa3
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2023-12-19 21:39:23 +0100

    Update Monitoring.class.php

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -39,7 +39,7 @@ class Monitoring extends eqLogic {
 
 	public static function pull() {
 		foreach (eqLogic::byType('Monitoring', true) as $Monitoring) {
-			if ($Monitoring->getConfiguration('maitreesclave') != 'local' && $Monitoring->getIsEnable()) {
+			if ($Monitoring->getConfiguration('maitreesclave') != 'local') {
 				log::add('Monitoring', 'debug', '[PULL] Lancement pull :: '. $Monitoring->getName());
 				$Monitoring->getInformations();
 				$mc = cache::byKey('MonitoringWidgetmobile' . $Monitoring->getId());
@@ -55,7 +55,7 @@ class Monitoring extends eqLogic {
 
 	public static function pullLocal() {
 		foreach (eqLogic::byType('Monitoring', true) as $Monitoring) {
-			if ($Monitoring->getConfiguration('maitreesclave') == 'local' && $Monitoring->getIsEnable()) {
+			if ($Monitoring->getConfiguration('maitreesclave') == 'local') {
 				log::add('Monitoring', 'debug', '[PULL] Lancement pullLocal :: '. $Monitoring->getName());
 				$Monitoring->getInformations();
 				$mc = cache::byKey('MonitoringWidgetmobile' . $Monitoring->getId());
