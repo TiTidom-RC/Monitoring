a4506bc6181dd8f775004d0a3c6b22baca862714
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-01-20 12:06:10 +0100

    Update cpu_temp visibility

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -580,7 +580,7 @@ class Monitoring extends eqLogic {
 		$cpu_temp = $this->getCmd(null,'cpu_temp');
 		$replace['#cpu_temp#'] = (is_object($cpu_temp)) ? $cpu_temp->execCmd() : '';
 		$replace['#cpu_tempid#'] = is_object($cpu_temp) ? $cpu_temp->getId() : '';
-		$replace['#cpu_temp_display#'] = (is_object($cpu_temp) && $cpu_temp->getIsVisible()) ? 'Yes' : 'No';
+		$replace['#cpu_temp_display#'] = (is_object($cpu_temp) && $cpu_temp->getIsVisible()) ? 'OK' : '';
 
 		$perso1 = $this->getCmd(null,'perso1');
 		$replace['#perso1#'] = (is_object($perso1)) ? $perso1->execCmd() : '';
