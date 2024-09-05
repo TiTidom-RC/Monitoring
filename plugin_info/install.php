9c1c6ea91c8dd19a9d344e6ef3eac6e63cb56e32
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-09-05 09:23:06 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -63,8 +63,8 @@ function Monitoring_update() {
     config::save('pluginVersion', $pluginVersion, 'Monitoring');
 
     // Check Version of the plugin
-    log::add('Monitoring', 'debug', '[UPDATE_CHECK] Vérification des versions :: ' . jeedom::version() . ' vs ' . '4.4' . ' :: ' . version_compare(jeedom::version(), '4.4'));
-    if (version_compare(jeedom::version(), '4.4') == -1) {
+    log::add('Monitoring', 'debug', '[UPDATE_CHECK] Vérification des versions :: ' . jeedom::version() . ' vs ' . '4.5' . ' :: ' . version_compare(jeedom::version(), '4.5'));
+    if (version_compare(jeedom::version(), '4.5') == -1) {
         $updateConf = update::byLogicalId('Monitoring');
         if (is_object($updateConf)) {
             $updateConf->setConfiguration('doNotUpdate', 1);
