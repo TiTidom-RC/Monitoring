3fb2dea3e2d518d26a56c4539db517c0b948428d
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-10-02 11:30:13 +0200

    Update Tendance

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -715,11 +715,11 @@ class Monitoring extends eqLogic {
 			$startHist = date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' -' . config::byKey('historyCalculTendance') . ' hour'));
 			$tendance = $loadavg1mn->getTendance($startHist, date('Y-m-d H:i:s'));
 			if ($tendance > config::byKey('historyCalculTendanceThresholddMax')) {
-				$replace['#loadavg1mn_tendance#'] = '<i class=\"fas fa-arrow-up\"></i>';
+				$replace['#loadavg1mn_tendance#'] = '<i class="fas fa-arrow-up"></i>';
 			} else if ($tendance < config::byKey('historyCalculTendanceThresholddMin')) {
-				$replace['#loadavg1mn_tendance#'] = '<i class=\"fas fa-arrow-down\"</i>';
+				$replace['#loadavg1mn_tendance#'] = '<i class="fas fa-arrow-down"</i>';
 			} else {
-				$replace['#loadavg1mn_tendance#'] = '<i class=\"fas fa-minus\"></i>';
+				$replace['#loadavg1mn_tendance#'] = '<i class="fas fa-minus"></i>';
 			}
 		}
 
