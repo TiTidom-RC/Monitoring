d364197a21e9ab1aeafa401c0b7fcc0f81977f74
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-10-08 18:34:30 +0200

    Update Monitoring.class.php

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -1829,12 +1829,10 @@ class Monitoring extends eqLogic {
 
 					// Perso1 Command
 					$perso1_cmd = $this->getConfiguration('perso1', '');
-					log::add('Monitoring','debug', '['. $equipement .'][SSH-CMD][PERSO1] Commande :: ' . str_replace("\r\n", "\\r\\n", $perso1_cmd));
 					$perso1 = trim($perso1_cmd) !== '' ? $this->execSSH($hostId, $perso1_cmd, 'Perso1') : '';
 
 					// Perso2 Command
 					$perso2_cmd = $this->getConfiguration('perso2', '');
-					log::add('Monitoring','debug', '['. $equipement .'][SSH-CMD][PERSO2] Commande :: ' . str_replace("\r\n", "\\r\\n", $perso2_cmd));
 					$perso2 = trim($perso2_cmd) !== '' ? $this->execSSH($hostId, $perso2_cmd, 'Perso2') : '';
 					
 					if ($this->getConfiguration('synology') == '1') {
@@ -2281,12 +2279,10 @@ class Monitoring extends eqLogic {
 				
 				// Perso1 Command
 				$perso1_cmd = $this->getConfiguration('perso1', '');
-				log::add('Monitoring','debug', '['. $equipement .'][LOCAL][PERSO1] Commande :: ' . str_replace("\r\n", "\\r\\n", $perso1_cmd));
 				$perso1 = trim($perso1_cmd) !== '' ? $this->execSRV($perso1_cmd, 'Perso1') : '';
 
 				// Perso2 Command
 				$perso2_cmd = $this->getConfiguration('perso2', '');
-				log::add('Monitoring','debug', '['. $equipement .'][LOCAL][PERSO2] Commande :: ' . str_replace("\r\n", "\\r\\n", $perso2_cmd));
 				$perso2 = trim($perso2_cmd) !== '' ? $this->execSRV($perso2_cmd, 'Perso2') : '';
 	
 				if ($ARMv == 'armv6l') {
