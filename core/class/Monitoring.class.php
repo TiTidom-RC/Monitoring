00609c763e8ebceecda49375f6b6f757d2ef19cb
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-10-08 11:03:23 +0200

    update dep install

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -35,97 +35,19 @@ class Monitoring extends eqLogic {
 	}
 
 	public static function dependancy_install() {
-
-		$php_file = __DIR__ . '/../php/Monitoringcli.php';
-		try {
-			if (!file_exists($php_file)) {
-				log::add('Monitoring', 'error', __('[DEP-INSTALL] Le fichier PHP n\'existe pas', __FILE__));
-			} else {
-				log::add('Monitoring', 'warning', __('[DEP-INSTALL] Lancement de l\'installation des dépendances', __FILE__));
-				$result = shell_exec('php ' . $php_file . ' depinstall 2>&1');
-				log::add('Monitoring', 'warning', __('[DEP-INSTALL] Résultat :: ', __FILE__) . $result);
-			}
-		} catch (Exception $e) {
-			log::add('Monitoring', 'error', __('[DEP-INSTALL] Erreur lors de l\'installation des dépendances :: ', __FILE__) . $e->getMessage());
-		}
-
 		$_logName = __CLASS__ . '_update';
 		log::add($_logName, 'info', '[DEP-INSTALL] Début des dépendances');
 		config::save('lastDependancyInstallTime', date('Y-m-d H:i:s'), plugin::byId('Monitoring')->getId());
 
 		try {
-			$_plugin = plugin::byId('sshmanager');
-			log::add($_logName, 'info', __('[DEP-INSTALL] Le plugin SSHManager est déjà installé', __FILE__));
-			if (!$_plugin->isActive()) {
-				log::add($_logName, 'error', __('[DEP-INSTALL] Le plugin SSHManager n\'est pas activé', __FILE__));
-				$_plugin->setIsEnable(1, true, true);
-				log::add($_logName, 'info', __('[DEP-INSTALL] Activation du plugin SSHManager', __FILE__));
-			} else {
-				log::add($_logName, 'info', __('[DEP-INSTALL] Plugin SSHManager :: actif', __FILE__));
+			log::add('Monitoring', 'warning', __('[DEP-INSTALL] Lancement de l\'installation des dépendances', __FILE__));
+			$result = shell_exec('php ' . __DIR__ . '/../php/Monitoringcli.php' . ' depinstall ' . $_logName . ' 2>&1');
+			if (!empty($result)) {
+				log::add('Monitoring', 'warning', __('[DEP-INSTALL] Résultat :: ', __FILE__) . $result);
 			}
-			log::add($_logName, 'info', '[DEP-INSTALL] Fin des dépendances');
 		} catch (Exception $e) {
-			log::add($_logName, 'warning', '[DEP-INSTALL] ' . $e->getMessage());
-			log::add($_logName, 'info', __('[DEP-INSTALL] Lancement de l\'installation du plugin SSHManager', __FILE__));
-
-			// Installation du plugin SSHManager (même version que celle du plugin Monitoring)
-			$_pluginSource = update::byLogicalId('Monitoring');
-			$_pluginToInstall = update::byLogicalId('sshmanager');
-			if (!is_object($_pluginToInstall)) {
-				$_pluginToInstall = new update();
-			}
-			$_pluginToInstall->setLogicalId('sshmanager');
-			$_pluginToInstall->setType('plugin');
-			$_pluginToInstall->setSource($_pluginSource->getSource());
-			if ($_pluginSource->getSource() == 'github') {
-				$_pluginToInstall->setConfiguration('user', $_pluginSource->getConfiguration('user'));
-				$_pluginToInstall->setConfiguration('repository', 'SSH-Manager');
-				if (strpos($_pluginSource->getConfiguration('version', 'stable'), 'dev') !== false) {
-					$_pluginToInstall->setConfiguration('version', 'dev');
-					log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: dev (GitHub)');
-				} else {
-					$_pluginToInstall->setConfiguration('version', $_pluginSource->getConfiguration('version', 'stable'));
-					log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: ' . $_pluginSource->getConfiguration('version', 'stable') . ' (GitHub)');
-				}
-				$_pluginToInstall->setConfiguration('token', $_pluginSource->getConfiguration('token'));
-			} else {
-				$_pluginToInstall->setConfiguration('version', $_pluginSource->getConfiguration('version', 'stable'));
-				log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: ' . $_pluginSource->getConfiguration('version', 'stable') . ' (Market)');
-			}
-			$_pluginToInstall->save();
-			$_pluginToInstall->doUpdate();
-			
-			// Vérification de l'installation du plugin SSHManager
-			$isNotInstalled = true;
-			$num = 30;
-			$_plugin = null;
-			while ($isNotInstalled && $num > 0) {
-				try {
-					$_plugin = plugin::byId('sshmanager');
-					$isNotInstalled = false;
-				} catch (Exception $e) {
-					log::add($_logName, 'debug', '[DEP-INSTALL] While Message (' . strval($num) . ') :: ' . $e->getMessage());
-					$num--;
-					sleep(1);
-				}
-			}
-			if ($num == 0) {
-				log::add($_logName, 'error', '[DEP-INSTALL] Le plugin SSHManager n\'a pas pu être installé !');
-			} else {
-				log::add($_logName, 'info', '[DEP-INSTALL] Le plugin SSHManager est maintenant installé');
-				if (is_object($_plugin)) {
-					try {
-						$_plugin->setIsEnable(1, true, true);
-						log::add($_logName, 'info', '[DEP-INSTALL] Le plugin SSHManager est maintenant activé');
-						jeedom::cleanFileSystemRight();
-						log::add($_logName, 'info', '[DEP-INSTALL] Fin des dépendances');
-					} catch (Exception $e) {
-						log::add($_logName, 'warning', '[DEP-INSTALL] Exception :: ' . $e->getMessage());
-						log::add($_logName, 'error', '[DEP-INSTALL] Le plugin SSHManager n\'a pas pu être installé !');
-					}
-				}
-			}
-		}	
+			log::add('Monitoring', 'error', __('[DEP-INSTALL] Erreur lors de l\'installation des dépendances :: ', __FILE__) . $e->getMessage());
+		}
         return array('log' => log::getPathToLog(__CLASS__ . '_update'));
     }
 
--- core/php/Monitoringcli.php
+++ core/php/Monitoringcli.php
@@ -25,12 +25,86 @@ if (!isset($argv[1])) {
     $argv[1] = '';
 }
 if (!isset($argv[2])) {
-    $argv[2] = '';
+    $argv[2] = 'Monitoring_update';
 }
+
+$_logName = $argv[2];
 
 switch ($argv[1]) {
     case 'depinstall':
-        log::add('Monitoring', 'warning', 'Dependancy install end for ' . $argv[1]);
+        try {
+			$_plugin = plugin::byId('sshmanager');
+			log::add($_logName, 'info', __('[DEP-INSTALL] Le plugin SSHManager est déjà installé', __FILE__));
+			if (!$_plugin->isActive()) {
+				log::add($_logName, 'error', __('[DEP-INSTALL] Le plugin SSHManager n\'est pas activé', __FILE__));
+				$_plugin->setIsEnable(1, true, true);
+				log::add($_logName, 'info', __('[DEP-INSTALL] Activation du plugin SSHManager', __FILE__));
+			} else {
+				log::add($_logName, 'info', __('[DEP-INSTALL] Plugin SSHManager :: actif', __FILE__));
+			}
+			log::add($_logName, 'info', '[DEP-INSTALL] Fin des dépendances');
+		} catch (Exception $e) {
+			log::add($_logName, 'warning', '[DEP-INSTALL] ' . $e->getMessage());
+			log::add($_logName, 'info', __('[DEP-INSTALL] Lancement de l\'installation du plugin SSHManager', __FILE__));
+
+			// Installation du plugin SSHManager (même version que celle du plugin Monitoring)
+			$_pluginSource = update::byLogicalId('Monitoring');
+			$_pluginToInstall = update::byLogicalId('sshmanager');
+			if (!is_object($_pluginToInstall)) {
+				$_pluginToInstall = new update();
+			}
+			$_pluginToInstall->setLogicalId('sshmanager');
+			$_pluginToInstall->setType('plugin');
+			$_pluginToInstall->setSource($_pluginSource->getSource());
+			if ($_pluginSource->getSource() == 'github') {
+				$_pluginToInstall->setConfiguration('user', $_pluginSource->getConfiguration('user'));
+				$_pluginToInstall->setConfiguration('repository', 'SSH-Manager');
+				if (strpos($_pluginSource->getConfiguration('version', 'stable'), 'dev') !== false) {
+					$_pluginToInstall->setConfiguration('version', 'dev');
+					log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: dev (GitHub)');
+				} else {
+					$_pluginToInstall->setConfiguration('version', $_pluginSource->getConfiguration('version', 'stable'));
+					log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: ' . $_pluginSource->getConfiguration('version', 'stable') . ' (GitHub)');
+				}
+				$_pluginToInstall->setConfiguration('token', $_pluginSource->getConfiguration('token'));
+			} else {
+				$_pluginToInstall->setConfiguration('version', $_pluginSource->getConfiguration('version', 'stable'));
+				log::add($_logName, 'info', '[DEP-INSTALL] Installation de la version :: ' . $_pluginSource->getConfiguration('version', 'stable') . ' (Market)');
+			}
+			$_pluginToInstall->save();
+			$_pluginToInstall->doUpdate();
+			
+			// Vérification de l'installation du plugin SSHManager
+			$isNotInstalled = true;
+			$num = 30;
+			$_plugin = null;
+			while ($isNotInstalled && $num > 0) {
+				try {
+					$_plugin = plugin::byId('sshmanager');
+					$isNotInstalled = false;
+				} catch (Exception $e) {
+					log::add($_logName, 'debug', '[DEP-INSTALL] While Message (' . strval($num) . ') :: ' . $e->getMessage());
+					$num--;
+					sleep(1);
+				}
+			}
+			if ($num == 0) {
+				log::add($_logName, 'error', '[DEP-INSTALL] Le plugin SSHManager n\'a pas pu être installé !');
+			} else {
+				log::add($_logName, 'info', '[DEP-INSTALL] Le plugin SSHManager est maintenant installé');
+				if (is_object($_plugin)) {
+					try {
+						$_plugin->setIsEnable(1, true, true);
+						log::add($_logName, 'info', '[DEP-INSTALL] Le plugin SSHManager est maintenant activé');
+						jeedom::cleanFileSystemRight();
+						log::add($_logName, 'info', '[DEP-INSTALL] Fin des dépendances');
+					} catch (Exception $e) {
+						log::add($_logName, 'warning', '[DEP-INSTALL] Exception :: ' . $e->getMessage());
+						log::add($_logName, 'error', '[DEP-INSTALL] Le plugin SSHManager n\'a pas pu être installé !');
+					}
+				}
+			}
+		}	
         break;
     default:
         help();
