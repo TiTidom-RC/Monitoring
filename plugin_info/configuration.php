da0cb8fe5c6e673e0b4b0b0b612f50a1d651b620
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-10-19 19:35:05 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -40,21 +40,34 @@ if (version_compare(jeedom::version(), '4.4', '<')) {
 $_versionSSHManager = config::byKey('pluginVersion', 'sshmanager', 'N/A');
 $_branchSSHManager = config::byKey('pluginBranch', 'sshmanager', 'N/A');
 
-?>
+$_branchMonitoring = config::byKey('pluginBranch', 'Monitoring', 'N/A');
 
-<form class="form-horizontal">
-    <fieldset>
-        <div>
-            <legend><i class="fas fa-info"></i> {{Plugin(s)}}</legend>
-            <div class="form-group">
-                <label class="col-md-3 control-label">{{Version Monitoring}}
-                    <sup><i class="fas fa-question-circle tooltips" title="{{Version du plugin Monitoring à indiquer sur Community}}"></i></sup>
-                </label>
+if (strpos($_branchMonitoring, 'stable') !== false) {
+    $_branchMonitoring = '<span class="label label-success">Stable</span>';
+} elseif (strpos($_branchMonitoring, 'beta') !== false) {
+    $_branchMonitoring = '<span class="label label-warning">Béta</span>';
+} elseif (strpos($_branchMonitoring, 'dev') !== false) {
+    $_branchMonitoring = '<span class="label label-danger">Dev</span>';
+} else {
+    $_branchMonitoring = '<span class="label label-default">N/A</span>';
+}
+
+if (strpos($_branchSSHManager, 'stable') !== false) {
+    $_branchSSHManager = '<span class="label label-success">Stable</span>';
+} elseif (strpos($_branchSSHManager, 'beta') !== false) {
+    $_branchSSHManager = '<span class="label label-warning">Béta</span>';
+} elseif (strpos($_branchSSHManager, 'dev') !== false) {
+    $_branchSSHManager = '<span class="label label-danger">Dev</span>';
+} else {
+    $_branchSSHManager = '<span class="label label-default">N/A</span>';
+}
+
+?>
                 <div class="col-md-1">
                     <input class="configKey form-control" data-l1key="pluginVersion" readonly />
                 </div>
                 <div class="col-md-1">
-                    <span class="configKey label label-info" data-l1key="pluginBranch"></span>
+                    <?php echo $_branchMonitoring ?>
                 </div>
             </div>
             <div class="form-group">
@@ -63,6 +76,9 @@ $_branchSSHManager = config::byKey('pluginBranch', 'sshmanager', 'N/A');
                 </label>
                 <div class="col-md-1">
                     <input class="form-control" value="<?php echo $_versionSSHManager ?>" readonly />
+                </div>
+                <div class="col-md-1">
+                    <?php echo $_branchSSHManager ?>
                 </div>
             </div>
             <legend><i class="fas fa-tasks"></i> {{Mises à jour Automatiques}} :</legend>
