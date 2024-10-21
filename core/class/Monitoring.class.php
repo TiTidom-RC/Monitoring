44a3b8b60a749ad766c3fe2968c302d1a6a982ce
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-10-21 15:02:15 +0200

    Update Monitoring.class.php

--- core/class/Monitoring.class.php
+++ core/class/Monitoring.class.php
@@ -2024,7 +2024,7 @@ class Monitoring extends eqLogic {
 				$replace[$cmdNamePrefix . '_averageHistory#'] = $isCmdObject ? $cmd->getConfiguration($cmdName . '_averageHistory') : '-';
 				$replace[$cmdNamePrefix . '_minHistory#'] = $isCmdObject ? $cmd->getConfiguration($cmdName . '_minHistory') : '-';
 				$replace[$cmdNamePrefix . '_maxHistory#'] = $isCmdObject ? $cmd->getConfiguration($cmdName . '_maxHistory') : '-';
-				$replace[$cmdNamePrefix . '_tendance#'] = ($isCmdObject && $this->getConfiguration('stats_tendance' == '1')) ? ' <i style="color: var(--al-info-color) !important;" class="fas fa-' . $cmd->getConfiguration($cmdName . '_tendance') . '"></i>' : '<i></i>';
+				$replace[$cmdNamePrefix . '_tendance#'] = ($isCmdObject && $this->getConfiguration('stats_tendance', '0') == '1') ? ' <i style="color: var(--al-info-color) !important;" class="fas fa-' . $cmd->getConfiguration($cmdName . '_tendance') . '"></i>' : '<i></i>';
 			}
 		];
 
